382eb6a738bc85b9ae023a0be4f1812eb97c9f3b
¨jonathansansok¨ <jonasans2@live.com.ar>
2023-01-17 15:38:55 -0300

    cart

--- services/notifications.service.js
+++ services/notifications.service.js
@@ -20,7 +20,7 @@ const notificationService = {
   notifyByEmailUser: async (nuevoUsuario) => {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
-      to: 'andres.fernandezcaballero@davinci.edu.ar', // Change to your recipient
+      to: process.env.ADMIN_EMAIL, // Change to your recipient
       from: process.env.ADMIN_EMAIL, // Change to your verified sender
       subject: "Nuevo usuario",
       text: `Nuevo usuario registrado ${nuevoUsuario.username} ${nuevoUsuario.email}`,
@@ -28,7 +28,7 @@ const notificationService = {
     };
     await sgMail
       .send(msg)
-      .then((response) => console.log("EMAIL BIEN ENVIADO" + response))
+      .then((response) => console.log("EMAIL BIEN ENVIADO " + response))
       .catch((error) => console.log(error));
   },
 };
