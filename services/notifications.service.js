a60578e37d75153d3825b252cdf4ef6adbe36951
¨jonathansansok¨ <jonasans2@live.com.ar>
2023-01-17 03:52:07 -0300

    whatsappbien

--- services/notifications.service.js
+++ services/notifications.service.js
@@ -6,16 +6,15 @@ const client = require("twilio")(accountSid, authToken);
 console.log(accountSid);
 const notificationService = {
   notifyByWhatsApp: async (nuevoUsuario) => {
-    await client.messages
-      .create({
-        body: `Tienes un nuevo usuario registrado ${nuevoUsuario.username} ${nuevoUsuario.email}`,
-        from: "whatsapp:+14155238886",
-        to: `whatsapp:${process.env.ADMIN_PHONE}`,
-      })
+    await client.messages.create({
+      body: `Tienes un nuevo usuario registrado con el nombre ${nuevoUsuario.username} y su email es ${nuevoUsuario.email}`,
+      from: "whatsapp:+14155238886",
+      to: `whatsapp:${process.env.ADMIN_PHONE}`,
+    });
   },
   notifyByEmailCompra: (asunto, listaProductos, usuarioComprador) => {},
-  notifyByEmailUser: async(nuevoUsuario) => {
-     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  notifyByEmailUser: async (nuevoUsuario) => {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
       to: process.env.ADMIN_EMAIL, // Change to your recipient
       from: process.env.ADMIN_EMAIL, // Change to your verified sender
@@ -23,11 +22,10 @@ const notificationService = {
       text: `Nuevo usuario registrado ${nuevoUsuario.username} ${nuevoUsuario.email}`,
       html: "<strong>and easy to do anywhere, even with Node.js</strong>",
     };
-    await sgMail  
+    await sgMail
       .send(msg)
-      .then(response => console.log('email has been seent'))
-      .catch(error => console.log(error.message)) 
-
+      .then((response) => console.log("EMAIL BIEN ENVIADO"))
+      .catch((error) => console.log(error.message));
   },
 };
 module.exports = notificationService;
