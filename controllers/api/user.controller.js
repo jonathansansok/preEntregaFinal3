80a92637aae70b483ec2b4f1dd8d17c951430578
¨jonathansansok¨ <jonasans2@live.com.ar>
2023-01-31 00:00:41 -0300

    js

--- controllers/api/user.controller.js
+++ controllers/api/user.controller.js
@@ -1,5 +1,23 @@
 const userModel = require("../../models/user.model");
 
+const UsersService = require ("../services/userService.js");
+const UserDTO = require ("../dtos/userDTO.js");
+const userService = new UsersService()
+
+const getUsers = async(req, res) => {
+  let result = await userService.getUsers()
+  let resultDTO = result.map(user => new UserDTO(user))
+  res.send(resultDTO)
+}
+
+const saveUser = async (req, res) => {
+  let user = req.body
+  //TODO: Validaciones...
+  let result = await userService.addUser(user)
+  res.send(new UserDTO(result))
+}
+
+
 const userController = {
   getAllUsers: async (req, res) => {
     try {
@@ -19,4 +37,4 @@ const userController = {
   },
 };
 
-module.exports = userController;
+module.exports = getUsers, saveUser, userController;
